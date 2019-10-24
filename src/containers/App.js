4e14e93cffbf356dba3ceb7aafc7a7213e7266ed
github username <josephspinelli@yahoo.com>
2019-10-24 12:33:27 -0500

    working

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import { observer, inject } from 'mobx-react'; //These functions make our components observable and enable them to use the store.
+import TodoList from '../components/TodoList';
+import Form from '../components/Form';
+import logo from '../logo.svg';
 import '../App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    const { store } = this.props;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div>
+          <h3 className="subtitle">Make a new To do</h3>
+          <Form store={store}/>
+        </div>
+        <div className="card-container">
+          {store.Todo.map((todo, i) => (
+            <TodoList todo={todo} key={i}/>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+// Now lets setup our App to receive the store so we can use the functionality we just built. We do this by
+// injecting the store into the components props with the inject method. Now we are able use it as we do with
+// <Form store={store}/> as well as with the mapping of our<TodoList todo={todo} key={i} /> component.
+export default inject('store')(observer (App));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import TaskStore from './models/TaskStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//debugging tools
+// import { onPatch } from 'mobx-state-tree';
+// import makeInspectable from 'mobx-devtools-mst';
 
+// onPatch(store, patch => {
+//   console.log(patch)
+// })
+const store = TaskStore.create({})
+// makeInspectable(store);
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
